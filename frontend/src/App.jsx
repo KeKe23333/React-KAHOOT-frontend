9ebfa70cb3cccbe75f811da311adaadff56be69e
Ke Wu <wuke00000@gmail.com>
2023-04-03 18:02:34 +0800

    first work day, creat login and register page, will abstract some components soon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+
+const About = () => {
+  return <div>About</div>;
+};
+
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
